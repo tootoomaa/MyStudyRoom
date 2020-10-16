9c6c642321f18e980c2784e569fa3ac466ca8c45
tootoomaa <tootoomaa@naver.com>
2020-10-16 14:56:21 +0900

    feat: adapted bind

--- MasteringRxSwift/souceCode/GoodWeather/GoodWeather/Controller/ViewController.swift
+++ MasteringRxSwift/souceCode/GoodWeather/GoodWeather/Controller/ViewController.swift
@@ -55,17 +55,26 @@ class ViewController: UIViewController {
     
     let resource = Resource<WeatherResult>(url: url)
     
-    URLRequest.load(resource: resource)
+    let search = URLRequest.load(resource: resource)
       .observeOn(MainScheduler.instance)
       .catchErrorJustReturn(WeatherResult.empty)
-      .subscribe(onNext:{ result in
-        
-        print(result)
-        
-        let weather = result.main
-        self.displayWeather(weather)
-        
-      }).disposed(by: disposeBag)
+    
+    search.map { "\($0.main.temp)℉" }
+      .bind(to: self.temperatureLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+    search.map { "\($0.main.humidity)💦"}
+      .bind(to: self.humidityLabel.rx.text)
+      .disposed(by: disposeBag)
+    
+//      .subscribe(onNext:{ result in
+//
+//        print(result)
+//
+//        let weather = result.main
+//        self.displayWeather(weather)
+//
+//      }).disposed(by: disposeBag)
   }
   
   private func displayWeather(_ weather: Weather?) {
