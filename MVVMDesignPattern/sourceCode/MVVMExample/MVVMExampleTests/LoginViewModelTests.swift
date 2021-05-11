fed826be54db7c5e55d21869247f4f1f66a5a5b0
tootoomaa <tootoomaa@naver.com>
2021-05-11 23:44:02 +0900

    feat: MVVM LoginViewModel 테스트 케이스 추가

---
+++ MVVMDesignPattern/sourceCode/MVVMExample/MVVMExampleTests/LoginViewModelTests.swift
@@ -0,0 +1,61 @@
+//
+//  LoginViewModelTests.swift
+//  MVVMExampleTests
+//
+//  Created by 김광수 on 2021/05/11.
+//
+
+import XCTest
+import Combine
+
+@testable import MVVMExample
+class LoginViewModelTests: XCTestCase {
+    
+    var sut: LoginViewModel!
+    var cancellable: Set<AnyCancellable>!
+    
+    override func setUpWithError() throws {
+        sut = LoginViewModel()
+        cancellable = Set<AnyCancellable>()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testLoginViewModel_WhenProvideEmptyEmail_ShouldReturnTrue() {
+        
+        // arrange
+        sut.email = ""
+        sut.password = "123213"
+        
+        // assert
+        sut.$isDisable
+            .sink { isDisable in
+                XCTAssertTrue(isDisable, "로그인 버튼 비활성화")
+            }.store(in: &cancellable)
+    }
+    
+    func testLoginViewModel_WhenProvideEmptyPassword_ShouldReturnTrue() {
+        // arrange
+        sut.email = "12321321"
+        sut.password = ""
+        
+        // assert
+        sut.$isDisable
+            .sink { isDisable in
+                XCTAssertTrue(isDisable, "로그인 버튼 비활성화")
+            }.store(in: &cancellable)
+    }
+    
+    func testLoginViewModel_WhenProviededPasswordAndEmail_ShoudlReturnFalse() {
+        // arrange
+        sut.email = "123213123"
+        sut.password = "123123123"
+        
+        sut.$isDisable
+            .sink { isDisable in
+                XCTAssertFalse(isDisable, "로그인 버튼 활성화")
+            }.store(in: &cancellable)
+    }
+}
--- MVVMDesignPattern/sourceCode/MVVMExample/MVVMExampleTests/MVCExampleTests.swift
+++ MVVMDesignPattern/sourceCode/MVVMExample/MVVMExampleTests/MVCExampleTests.swift
@@ -23,26 +23,26 @@ class MVCExampleTests: XCTestCase {
     /*
      Login button 은 true 일때 disable
      */
-    func testContentView_WhenUserEamilIsEmpty_LoginButtonStateShouldReturnTrue() {
-        
-        sut.email = ""
-        sut.password = "123123"
-        
-        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 비활성화")
-    }
-    
-    func testContentView_WhenUserPasswordIsEmpty_LoginButtonStateShouldReturnTrue() {
-        
-        sut.email = "123123"
-        sut.password = ""
-        
-        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 비활성화")
-    }
-    
-    func testContentView_WhenPasswordAndEmailisValid_LoginButtonStateShouldReturnFalse() {
-        sut.email = "123123"
-        sut.password = "123123"
-        
-        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 활성화")
-    }
+//    func testContentView_WhenUserEamilIsEmpty_LoginButtonStateShouldReturnTrue() {
+//        
+//        sut.email = ""
+//        sut.password = "123123"
+//        
+//        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 비활성화")
+//    }
+//    
+//    func testContentView_WhenUserPasswordIsEmpty_LoginButtonStateShouldReturnTrue() {
+//        
+//        sut.email = "123123"
+//        sut.password = ""
+//        
+//        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 비활성화")
+//    }
+//    
+//    func testContentView_WhenPasswordAndEmailisValid_LoginButtonStateShouldReturnFalse() {
+//        sut.email = "123123"
+//        sut.password = "123123"
+//        
+//        XCTAssertTrue(sut.email.isEmpty || sut.password.isEmpty, "로그인 버튼 활성화")
+//    }
 }
