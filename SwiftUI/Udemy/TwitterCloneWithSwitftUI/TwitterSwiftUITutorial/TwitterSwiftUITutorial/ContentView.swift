a4c8b2d0fcf145cb5af8641b18ae5846cfabae08
tootoomaa <tootoomaa@naver.com>
2021-06-21 09:04:10 +0900

    feat: TwitterColne Start with Udemy

---
+++ SwiftUI/Udemy/TwitterCloneWithSwitftUI/TwitterSwiftUITutorial/TwitterSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by 김광수 on 2021/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Udemy/TwitterCloneWithSwitftUI/TwitterSwiftUITutorial/TwitterSwiftUITutorial/TwitterSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUITutorialApp.swift
+//  TwitterSwiftUITutorial
+//
+//  Created by 김광수 on 2021/06/15.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
