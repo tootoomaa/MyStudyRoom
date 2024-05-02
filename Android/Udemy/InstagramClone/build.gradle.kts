045086bf222515bd3b2333fa126a35f0b6e8b703
tootoomaa <tootoomaa@naver.com>
2024-05-02 14:53:09 +0900

    feat: 일부 프로젝트 정리

---
+++ Android/Udemy/InstagramClone/build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    kotlin("kapt") version "1.9.0"
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+}
